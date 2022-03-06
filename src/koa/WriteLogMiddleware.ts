297b9b3af87dab2b857c1db3a28a35467d5493fe
fangqk1991 <me@fangqk.com>
2022-03-06 22:01:04 +0800

    v0.1.0 - 项目初始化

---
+++ src/koa/WriteLogMiddleware.ts
@@ -0,0 +1,69 @@
+import { Context } from 'koa'
+
+type NormalHandler = (ctx: Context) => void | Promise<void>
+type ErrorHandler = (ctx: Context, err: Error) => void | Promise<void>
+
+export class WriteLogMiddlewareBuilder {
+  private _skippingUris: { [p: string]: true }
+  private _normalHandler?: NormalHandler
+  private _errorHandler?: ErrorHandler
+
+  public constructor() {
+    this._skippingUris = {
+      '/api/health/ping': true,
+    }
+  }
+
+  public setNormalHandler(handler: NormalHandler) {
+    this._normalHandler = handler
+    return this
+  }
+
+  public setErrorHandler(handler: ErrorHandler) {
+    this._errorHandler = handler
+    return this
+  }
+
+  public build() {
+    /**
+     * @description 请确保 ctx.logger 已被赋予 Logger 对象
+     */
+    return async (ctx: Context, next: Function) => {
+      const { method, url, hostname } = ctx.request
+
+      if (!this._skippingUris[url]) {
+        ctx.logger.info(`[Start] 200 0 "${method} ${url} ${hostname}"`)
+      }
+
+      const start = Date.now()
+      try {
+        await next()
+
+        const { method, url, hostname } = ctx.request
+        ctx.duration = Date.now() - start
+        if (!this._skippingUris[url]) {
+          ctx.logger.info(`[Completed] ${ctx.status} ${ctx.duration / 1000} "${method} ${url} ${hostname}"`)
+          if (this._normalHandler) {
+            await this._normalHandler(ctx)
+          }
+        }
+      } catch (err: any) {
+        ctx.status = err.statusCode || 500
+        ctx.body = err.message
+
+        const { method, url, hostname } = ctx.request
+        ctx.duration = Date.now() - start
+
+        if (this._errorHandler) {
+          await this._errorHandler(ctx, err)
+        }
+        ctx.logger.info(`[Completed] ${ctx.status} ${ctx.duration / 1000} "${method} ${url} ${hostname}"`)
+        ctx.logger.error(err.message)
+        ctx.logger.error(err)
+        ctx.app.emit('error', err, ctx)
+      }
+    }
+  }
+}
+
+export const WriteLogMiddleware = new WriteLogMiddlewareBuilder().build()
